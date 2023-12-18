ba58ada2c8a96c0d7e812da15e3978f15fc5174e
Brent8091 <809.brent.809@gmail.com>
2023-12-18 15:14:33 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bin2Dec';
+  binaryForm: FormGroup;
+  givenNumber!: string;
+  parsedNumber!: number;
+
+  constructor(private fb: FormBuilder) {
+    this.binaryForm = fb.group({
+      BinaryNumber: ['', Validators.required, Validators.min(1)]
+    })
+  }
+
+  Convert(givenNumber: string) {
+    const number = givenNumber.toString();
+    console.log(number)
+    const parsedNumber = parseInt(number, 2);
+    console.log(parsedNumber)
+    this.parsedNumber = parsedNumber;
+    console.log(this.parsedNumber)
+  }
 }
